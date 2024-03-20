7d6958c0e7dd2561150aa4f00f96bc711448c929
shahinexy <shahinexy@gmail.com>
2024-03-20 11:55:50 +0600

    prop types validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import DaisyNav from "./component/DaisyNav/DaisyNav"
+import NavBar from "./component/NavBar/NavBar"
 
 
 function App() {
 
   return (
     <>
+      <NavBar></NavBar>
       <DaisyNav></DaisyNav>
     </>
   )
